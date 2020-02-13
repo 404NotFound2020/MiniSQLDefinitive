7055cf052ef9c4fe5e304d907be3e9f10eed6aff
Aitor Ispas <aitorcuv@gmail.com>
2020-02-13 13:11:19 +0100

    Commit interface and two definitions of classes.

---
+++ MiniSQL/Parsers/XMLParser.cs
@@ -0,0 +1,27 @@
+﻿using MiniSQL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniSQL.Parsers
+{
+    class XMLParser : IParser
+    {
+        public bool CreateDatabase(string databaseName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DropDatabase(string databaseName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Database GetDatabase(string databaseName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
