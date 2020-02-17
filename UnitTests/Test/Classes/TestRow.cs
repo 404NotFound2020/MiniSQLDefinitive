9149515d057844ea13b66099dff2c0d44591818d
Aitor Merino <iguan@PORTATIL-MERINO>
2020-02-17 13:14:37 +0100

    TestRow in testing

--- UnitTests/Test/Classes/TestRow.cs
+++ UnitTests/Test/Classes/TestRow.cs
@@ -15,7 +15,8 @@ namespace UnitTests.Test
         {
             List<string> dataToWork = TestColumn.CreateStringCellData();
             Table t = new Table("Test");
-            t.AddColumn(new Column("P1",tdataType));
+            Column column = ObjectConstructor.CreateColumn(dataToWork, TypesKeyConstants.StringTypeKey, "P1");
+            t.AddColumn(column);
             t.AddRow(r);
             r.AddCell(new Cell(t.GetColumn("P1"), "D1", r));
             r.AddCell(new Cell(t.GetColumn("P1"), "D2", r));
@@ -29,98 +30,34 @@ namespace UnitTests.Test
 
     
     [TestMethod]
-        public void ExistCells_Exist_ReturnTrue_StrType()
+        public void ExistCells_Exist_ReturnTrue()
         {
             Row row = new Row();
             row = TestRow.CreateStringCellData(row);
             Assert.IsNotNull(row.GetCell("P1"));
-
-            Assert.IsTrue(dataToWork.Count > 0);
-            Assert.IsTrue(column.ExistCells(dataToWork[0]));
         }
         [TestMethod]
         public void AddCell(Cell cell)
         {
+            Row row = new Row();
+            row = TestRow.CreateStringCellData(row);
+            row.AddCell(cell);
+            Assert.IsNotNull(row.GetCell(cell.column.columnName));
         }
         [TestMethod]
         public void GetCell(string columnName)
         {
+            Row row = new Row();
+            List<string> dataToWork = TestColumn.CreateStringCellData();
+            Table t = new Table("Test");
+            Column column = ObjectConstructor.CreateColumn(dataToWork, TypesKeyConstants.StringTypeKey, columnName);
+            t.AddColumn(column);
+            t.AddRow(row);
+            row.AddCell(new Cell(t.GetColumn("P1"), "D1", row));
+
+            Assert.IsNotNull(row.GetCell(columnName));
         }
 
     }
-    ___________
-
-
-            [TestMethod]
-            public void ExistCells_Exist_ReturnTrue_StrType()
-            {
-                List<string> dataToWork = TestColumn.CreateStringCellData();
-                Column column = ObjectConstructor.CreateColumn(dataToWork, TypesKeyConstants.StringTypeKey, "aaa");
-                Assert.IsTrue(dataToWork.Count > 0);
-                Assert.IsTrue(column.ExistCells(dataToWork[0]));
-            }
-
-            [TestMethod]
-            public void ExistCells_NoExist_ReturnFalse_StrType()
-            {
-                List<string> dataToWork = TestColumn.CreateStringCellData();
-                Column column = ObjectConstructor.CreateColumn(dataToWork, TypesKeyConstants.StringTypeKey, "aaaa");
-                string randomStr = "uwu";
-                while (dataToWork.Contains(randomStr))
-                {
-                    randomStr = VariousFunctions.GenerateRandomString(8);
-                }
-                Assert.IsFalse(dataToWork.Contains(randomStr));
-                Assert.IsTrue(dataToWork.Count > 0);
-                Assert.IsFalse(column.ExistCells(randomStr));
-            }
-
-            [TestMethod]
-            public void AddCell_NoExistsCellsWithSameData_StrType()
-            {
-                List<string> dataToWork = TestColumn.CreateStringCellData();
-                Column column = ObjectConstructor.CreateColumn(dataToWork, TypesKeyConstants.StringTypeKey, "aaaa");
-                string randomStr = "uwu";
-                while (dataToWork.Contains(randomStr))
-                {
-                    randomStr = VariousFunctions.GenerateRandomString(8);
-                }
-                Assert.IsFalse(dataToWork.Contains(randomStr));
-                Assert.IsFalse(column.ExistCells(randomStr));
-                column.AddCell(ObjectConstructor.CreateCell(column, randomStr, null));
-                Assert.IsTrue(column.ExistCells(randomStr));
-            }
-
-            [TestMethod]
-            public void AddCell_ExistCellsWithSameData_StrType()
-            {
-                List<string> dataToWork = TestColumn.CreateStringCellData();
-                Column column = ObjectConstructor.CreateColumn(dataToWork, TypesKeyConstants.StringTypeKey, "aaaa");
-                int index = 0;
-                Assert.IsTrue(dataToWork.Count > 0);
-                Assert.IsTrue(column.ExistCells(dataToWork[index]));
-                int numberOfCellWithSameDataBeforeAddition = column.GetCells(dataToWork[index]).Count;
-                column.AddCell(ObjectConstructor.CreateCell(column, dataToWork[index], null));
-                Assert.AreEqual(numberOfCellWithSameDataBeforeAddition + 1, column.GetCells(dataToWork[index]).Count);
-            }
-
-            public static List<string> CreateStringCellData()
-            {
-                List<string> cellData = new List<string>();
-                cellData.Add("P1");
-                cellData.Add("P2");
-                cellData.Add("P3");
-                cellData.Add("P4");
-                cellData.Add("P5");
-                cellData.Add("P6");
-                cellData.Add("P7");
-                return cellData;
-            }
-
-
-
-
-
-
 }
 
