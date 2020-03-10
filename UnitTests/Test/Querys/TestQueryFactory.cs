b1d342476fd29deb5ed0df7b4d839be8d043e8bf
AitorIs <dispas001@ikasle.ehu.eus>
2020-03-10 14:52:20 +0100

    Shit, I had put the operator factory tests in TestQueryFactory

--- UnitTests/Test/Querys/TestQueryFactory.cs
+++ UnitTests/Test/Querys/TestQueryFactory.cs
@@ -1,42 +1,13 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using MiniSQL.Constants;
-using MiniSQL.Querys;
+
 
 namespace UnitTests.Test.Querys
 {
     [TestClass]
     public class TestQueryFactory
     {
-        [TestMethod]
-        public void TryingToGetEqualOperator()
-        {
-            OperatorFactory factory = GetOperatorFactory();
-            Operator op = factory.GetOperator(OperatorKeys.EqualKey);
-            Assert.IsTrue(op.IsSameOperator(Operator.equal));
-        }
-
-        [TestMethod]
-        public void TryingToGetLessOperator()
-        {
-            OperatorFactory factory = GetOperatorFactory();
-            Operator op = factory.GetOperator(OperatorKeys.LessKey);
-            Assert.IsTrue(op.IsSameOperator(Operator.less));
-
-        }
-
-        [TestMethod]
-        public void TryingToGetHigherOperator()
-        {
-            OperatorFactory factory = GetOperatorFactory();
-            Operator op = factory.GetOperator(OperatorKeys.HigherKey);
-            Assert.IsTrue(op.IsSameOperator(Operator.higher));
-        }
-
-        public static OperatorFactory GetOperatorFactory() 
-        {
-            return OperatorFactory.GetOperatorFactory();
-        }
+  
 
     }
 }
