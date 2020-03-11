b7ab82d0b3922f214fd30aa5eeb2fe08e030e72b
AitorIs <dispas001@ikasle.ehu.eus>
2020-03-11 19:16:27 +0100

    Commit select #43

--- ClientConsole/ConsoleStuff/QueryVerifier.cs
+++ ClientConsole/ConsoleStuff/QueryVerifier.cs
@@ -9,16 +9,34 @@ namespace ClientConsole.ConsoleStuff
     public class QueryVerifier
     {
         private static QueryVerifier queryVerifier;
-        private List<Tuple<string, Func<string, string>>> protocolesAndTheirConsolePatterns;
+        public List<Tuple<string, Func<string, string>>> protocolesAndTheirConsolePatterns;
+        public static string NAINCG = "[^\\* ,<=>]";
+        public static string wherePatern = "WHERE " + NAINCG + "+[<=>]" + NAINCG + "+;";
+        public static string fromPattern = "FROM (?:("+NAINCG+"+;)|(" + NAINCG + "+ "+ wherePatern+"))";
+        public static string selectPattern = "^SELECT (?:(\\*)|(" + NAINCG + "+)(?:,(" + NAINCG + "+))*) " + fromPattern + "$";
 
+        /**
+         * SELECT (?:(\*)|([^\* ,<=>]+)(?:,([^\* ,<=>]+))*) FROM (?:([^\* ,<=>]+;)|([^\* ,<=>]+ WHERE [^\* ,<=>]+[<=>][^\* ,<=>]+;))
+         */
         private QueryVerifier() 
         {
-            this.protocolesAndTheirConsolePatterns = new List<Tuple<string, Func<string, string>>>();
+            this.protocolesAndTheirConsolePatterns = new List<Tuple<string, Func<string, string>>>();        
         }
 
-        public static QueryVerifier GetQueryVerifier() 
+        private void AddTheProtocolesFunctions() 
         { 
         
+        
         }
+
+       
+
+
+        public static QueryVerifier GetQueryVerifier() 
+        {
+            if (queryVerifier == null) queryVerifier = new QueryVerifier();
+            return queryVerifier;
+        }
+
     }
 }
