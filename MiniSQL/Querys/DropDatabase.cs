98b6bb0ae9b468a672deb7367db11579116f7ea5
Jessica Santos <jessi_mn10@hotmail.com>
2020-03-16 12:06:04 +0100

    CreateDatabase, DropDatabase

---
+++ MiniSQL/Querys/DropDatabase.cs
@@ -0,0 +1,27 @@
+﻿using MiniSQL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniSQL.Querys
+{
+    public class DropDatabase : AbstractQuery
+    {
+        public DropDatabase(IDatabaseContainer container) : base(container)
+        {  
+
+        }
+
+        public override void Execute()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool ValidateParameters()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
