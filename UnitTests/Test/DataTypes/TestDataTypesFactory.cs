3666723ea186b52b938c6e26299c198707fd1a3a
Jessica Santos <jessi_mn10@hotmail.com>
2020-03-17 17:46:49 +0100

    testDataTypesFactory

--- UnitTests/Test/DataTypes/TestDataTypesFactory.cs
+++ UnitTests/Test/DataTypes/TestDataTypesFactory.cs
@@ -1,21 +1,42 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MiniSQL.Clases;
+using MiniSQL.Constants;
+using MiniSQL.DataTypes;
 
 namespace UnitTests.Test.DataTypes
 {
     [TestClass]
     public class TestDataTypesFactory
     {
-        [TestMethod]
-        public void TestGetDataTypesFactory()
-        {
 
+        [TestMethod]
+        public void TestGetStringTypeDataType()
+        {
+            DataTypesFactory dataTypesFactory = GetDataTypesFactory();
+            StringType stringType = StringType.GetStringType();
+            Assert.AreEqual(stringType, dataTypesFactory.GetDataType(TypesKeyConstants.StringTypeKey));
         }
 
         [TestMethod]
-        public void TestGetDataType() 
-        { 
-        
+        public void TestGetIntTypeDataType()
+        {
+            DataTypesFactory dataTypesFactory = GetDataTypesFactory();
+            IntType intType = IntType.GetIntType();
+            Assert.AreEqual(intType, dataTypesFactory.GetDataType(TypesKeyConstants.IntTypeKey));
+        }
+
+        [TestMethod]
+        public void TestGetDoubleTypeDataType()
+        {
+            DataTypesFactory dataTypesFactory = GetDataTypesFactory();
+            DoubleType doubleType = DoubleType.GetDoubleType();
+            Assert.AreEqual(doubleType, dataTypesFactory.GetDataType(TypesKeyConstants.DoubleTypeKey));
+        }
+
+        public static DataTypesFactory GetDataTypesFactory()
+        {
+            return DataTypesFactory.GetDataTypesFactory();
         }
 
     }
