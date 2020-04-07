05d33af5d9fa25fd7a6af0afce267bfdc5f046c4
AitorIs <dispas001@ikasle.ehu.eus>
2020-04-07 17:27:08 +0200

    Commit Close #57

--- UnitTests/Test/Querys/TestDelete.cs
+++ UnitTests/Test/Querys/TestDelete.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MiniSQL.Classes;
+using MiniSQL.Constants;
+using MiniSQL.DataTypes;
 using MiniSQL.Interfaces;
 using MiniSQL.Querys;
 using UnitTests.Test.TestObjectsContructor;
@@ -56,6 +58,46 @@ namespace UnitTests.Test.Querys
             delete.whereClause.AddCritery(new Tuple<string, string>("x", "a"), Operator.equal);
             Assert.IsFalse(delete.ValidateParameters());
             Assert.AreEqual(0, delete.GetAfectedRowCount());
+        }
+
+        [TestMethod]
+        public void DeleteRow_TheRowIsReferedForAnotherRows_DontDeleteTheRowRefered() 
+        {
+            //Construction phase
+            IDatabaseContainer container = ObjectConstructor.CreateDatabaseContainer();
+            Database db = new Database("database1");
+            Table table1 = new Table("table1");
+            Column column1t1 = new Column("c1t1", DataTypesFactory.GetDataTypesFactory().GetDataType(TypesKeyConstants.IntTypeKey));
+            table1.AddColumn(column1t1);
+            table1.primaryKey.AddKey(column1t1);
+            db.AddTable(table1);
+            Table table2 = new Table("table2");
+            Column column1t2 = new Column("c1t2", DataTypesFactory.GetDataTypesFactory().GetDataType(TypesKeyConstants.IntTypeKey));
+            table2.AddColumn(column1t2);
+            table2.primaryKey.AddKey(column1t2);
+            db.AddTable(table2);
+            container.AddDatabase(db);            
+            table2.foreignKey.AddForeignKey(column1t2, column1t1);
+            //Insert some data
+            Row rowT1;
+            int limit = 10;
+            for(int i = 0; i <= limit; i++) 
+            {
+                rowT1 = table1.CreateRowDefinition();
+                rowT1.GetCell(column1t1.columnName).data = "" + i;
+                table1.AddRow(rowT1);
+            }
+            Row rowT2 = table2.CreateRowDefinition();
+            rowT2.GetCell(column1t2.columnName).data = "" + limit;
+            table2.AddRow(rowT2);
+            //Test
+            int numberOfRows = table1.GetRowCount();
+            Delete delete = CreateDelete(container, db.databaseName, table1.tableName);
+            delete.whereClause.AddCritery(new Tuple<string, string>(column1t1.columnName, "" + 11), Operator.less);
+            Assert.IsTrue(delete.ValidateParameters());
+            delete.Execute();
+            Assert.AreEqual(table2.GetRowCount(), numberOfRows - delete.GetAfectedRowCount());
+            Assert.IsTrue(table1.GetColumn(column1t1.columnName).ExistCells(rowT2.GetCell(column1t2.columnName).data));
         }
 
         public Delete CreateDelete(IDatabaseContainer container, string databaseName, string tableName)
