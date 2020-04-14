09a3821ee952a40bcf3082fcd0c074bcebbb51e2
Aitor Merino <iguan@PORTATIL-MERINO>
2020-04-14 15:14:49 +0200

    grant privilege #67

--- MiniSQL/Querys/GrantPrivilege.cs
+++ MiniSQL/Querys/GrantPrivilege.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MiniSQL.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,42 @@ using System.Threading.Tasks;
 
 namespace MiniSQL.Querys
 {
-    public class GrantPrivilege
+    public class GrantPrivilege : PrivilegeManipulationQuery
     {
+        private string databaseName;
+        private string tableName;
+        private string privilegeName;
+        private string profileName;
+
+        public GrantPrivilege(IDatabaseContainer container) : base(container)
+        {
+           
+        }
+
+    public override void ExecuteParticularQueryAction()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetNeededExecutePrivilege()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool ValidateParameters()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetData()
+        {
+                    private string databaseName;
+        private string tableName;
+        private string privilegeName;
+        private string profileName;
+
     }
+    }
+
+
 }
