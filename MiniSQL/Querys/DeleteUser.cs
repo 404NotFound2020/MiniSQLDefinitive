2b845a2dd76e6b02fca96cc4af56e61f0f8f0954
Jessica Santos <jessi_mn10@hotmail.com>
2020-04-15 15:52:03 +0200

    # 63 - added deletequery

--- MiniSQL/Querys/DeleteUser.cs
+++ MiniSQL/Querys/DeleteUser.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MiniSQL.Classes;
+using MiniSQL.Constants;
+using MiniSQL.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,45 @@ using System.Threading.Tasks;
 
 namespace MiniSQL.Querys
 {
-    class DeleteUser 
+    public class DeleteUser : PrivilegeManipulationQuery
     {
+        private string targetUsername;
+        public DeleteUser(IDatabaseContainer container) : base(container)
+        {
+        }
+
+        public override bool ValidateParameters()
+        {
+            Column column = this.GetContainer().GetDatabase(this.targetDatabase).GetTable(this.targetTableName).GetColumn(SystemeConstants.UsersNameColumnName);
+            if (!column.ExistCells(this.targetUsername)) this.SaveTheError("The username doenst exist");
+            else if (!column.GetCells(this.targetUsername)[0].row.CheckIfRowCouldBeChanged()) this.SaveTheError("The username can not delete, check if there any fk reference to this username");
+            return this.GetIsValidQuery();
+        }
+
+        public override void ExecuteParticularQueryAction()
+        {
+            bool b = false;
+            ITable table = this.GetContainer().GetDatabase(this.targetDatabase).GetTable(this.targetTableName);
+            IEnumerator<Row> rowEnumerator = table.GetRowEnumerator();
+            int i = -1;
+            while (rowEnumerator.MoveNext() && !b)
+            {
+                b = rowEnumerator.Current.GetCell(SystemeConstants.UsersNameColumnName).data.Equals(this.targetUsername);
+                i++;
+            }
+            rowEnumerator.Dispose();
+            table.DestroyRow(i);
+        }
+
+        public override string GetNeededExecutePrivilege()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetTargetUserName(string targetUsername)
+        {
+            this.targetUsername = targetUsername;
+        }
+
     }
 }
