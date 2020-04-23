3ce83ca179217b5f675bdec790d333294fd5bd08
AitorIs <dispas001@ikasle.ehu.eus>
2020-04-23 15:05:45 +0200

    Added budler and update to 0.0.0.1 the tester.

---
+++ Bundler/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bundler
+{
+    class Program
+    {
+        public static string RelPathToSolutionRootFolder = "../../../";
+        public static string morePath = "minisql-tester/bin/Release/";
+        public static string RootFolderInZip;
+
+        public static void Main()
+        {
+            List<string> files = new List<string>();
+            string version;
+            version = GetVersion(RelPathToSolutionRootFolder + morePath + "minisql-tester.exe");
+            RootFolderInZip = "OurShit/";
+            files.Add(RelPathToSolutionRootFolder + morePath + "minisql-tester.exe");
+            files.Add(RelPathToSolutionRootFolder + morePath + "ClientConsole.exe");
+            files.Add(RelPathToSolutionRootFolder + morePath + "MiniSQL.dll");
+            string outputFile = RelPathToSolutionRootFolder + "TesterReleases/" + "404-" + version + ".zip";
+            Console.WriteLine("Compressing files");
+            Compress(outputFile, files);
+            Console.WriteLine("Finished");
+        }
+
+        public static string GetVersion(string file)
+        {
+            FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(file);
+
+            return fvi.FileVersion;
+        }
+
+        public static void GetDependencies(string inFolder, string module, ref List<string> dependencyList, bool bRecursive = true)
+        {
+            string depName, modName;
+
+            Assembly assembly = Assembly.LoadFrom(inFolder + module);
+
+            foreach (AssemblyName assemblyName in assembly.GetReferencedAssemblies())
+            {
+                modName = assemblyName.Name + ".dll";
+                depName = inFolder + modName;
+                if (System.IO.File.Exists(depName) && !dependencyList.Exists(name => name == depName))
+                {
+                    dependencyList.Add(depName);
+                    if (bRecursive)
+                        GetDependencies(inFolder, modName, ref dependencyList, false);
+                }
+            }
+        }
+
+        public static List<string> GetFilesInFolder(string folder, bool bRecursive, string filter = "*.*")
+        {
+            List<string> files = new List<string>();
+
+            if (bRecursive)
+                files.AddRange(Directory.EnumerateFiles(folder, filter, SearchOption.AllDirectories));
+            else
+                files.AddRange(Directory.EnumerateFiles(folder));
+            return files;
+        }
+
+        public static void Compress(string outputFilename, List<string> files)
+        {
+            uint numFilesAdded = 0;
+            double totalNumFiles = (double)files.Count;
+            using (FileStream zipToOpen = new FileStream(outputFilename, FileMode.Create))
+            {
+                using (ZipArchive archive = new ZipArchive(zipToOpen, ZipArchiveMode.Update))
+                {
+                    foreach (string file in files)
+                    {
+                        if (System.IO.File.Exists(file))
+                        {
+                            archive.CreateEntryFromFile(file, RootFolderInZip + file.Substring((RelPathToSolutionRootFolder + morePath).Length));//a lo chapuzas lo de morepath
+                            numFilesAdded++;
+                        }
+                        else Console.WriteLine("Couldn't find file: {0}", file);
+
+                        Console.Write("\rProgress: {0:F2}%", 100.0 * ((double)numFilesAdded) / totalNumFiles);
+                    }
+                    Console.WriteLine("\nSaving {0} files in  {1}", numFilesAdded, Path.GetFullPath(outputFilename));
+                }
+            }
+        }
+    }
+}
--- Bundler/Properties/AssemblyInfo.cs
+++ Bundler/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada a un ensamblado.
-[assembly: AssemblyTitle("minisql-tester")]
+[assembly: AssemblyTitle("Bundler")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("minisql-tester")]
+[assembly: AssemblyProduct("Bundler")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
-[assembly: Guid("a638ebde-7228-4be6-9237-57ba2772fd38")]
+[assembly: Guid("69405534-7229-4a57-913e-a89c080914ec")]
 
 // La información de versión de un ensamblado consta de los cuatro valores siguientes:
 //
@@ -33,5 +33,4 @@ using System.Runtime.InteropServices;
 // utilizando el carácter "*", como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("0.0.0.1")]
-[assembly: AssemblyInformationalVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
