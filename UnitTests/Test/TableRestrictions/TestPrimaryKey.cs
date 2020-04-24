224c3f601e8b39c6e41cee991874f156ae1b1431
Jessica Santos <jessi_mn10@hotmail.com>
2020-04-24 14:17:10 +0200

    # 53 - Test PrimaryKey

--- UnitTests/Test/TableRestrictions/TestPrimaryKey.cs
+++ UnitTests/Test/TableRestrictions/TestPrimaryKey.cs
@@ -44,6 +44,75 @@ namespace UnitTests.Test.TableRestrictions
             Assert.IsFalse(table.primaryKey.Evaluate(row));
         }
 
+
+        [TestMethod]
+        public void TwoColumnPk_InsertNewRowWithNoDuplicatedKey_EvalReturnTrue()
+        {
+            //Construct phase
+            Table table = CreatePrimaryKeysTable();
+            Column pkColumn1 = table.GetColumn(testTableColumns[0]);
+            Column pkColumn2 = table.GetColumn(testTableColumns[1]);
+            table.primaryKey.AddKey(pkColumn1);
+            table.primaryKey.AddKey(pkColumn2);
+            string noDuplicatedKey1 = ShittyUtilities.GenerateADoenstExistNewValue(pkColumn1);
+            string noDuplicatedKey2 = ShittyUtilities.GenerateADoenstExistNewValue(pkColumn2);
+            //Test
+            Row row = table.CreateRowDefinition();
+            row.GetCell(pkColumn1.columnName).data = noDuplicatedKey1;
+            row.GetCell(pkColumn2.columnName).data = noDuplicatedKey2;
+            Assert.IsTrue(table.primaryKey.Evaluate(row));
+        }
+
+        [TestMethod]
+        public void TwoColumnPk_InsertNewRowWithDuplicatedKey_EvalReturnTrue()
+        {
+            //Construct phase
+            Table table = CreatePrimaryKeysTable();
+            Column pkColumn1 = table.GetColumn(testTableColumns[0]);
+            Column pkColumn2 = table.GetColumn(testTableColumns[1]);
+            table.primaryKey.AddKey(pkColumn1);
+            table.primaryKey.AddKey(pkColumn2);
+            string noDuplicatedKey1 = ShittyUtilities.GenerateADoenstExistNewValue(pkColumn1);
+            string noDuplicatedKey2 = ShittyUtilities.GenerateADoenstExistNewValue(pkColumn2);
+            Row row = table.CreateRowDefinition();
+            row.GetCell(pkColumn1.columnName).data = noDuplicatedKey1;
+            row.GetCell(pkColumn2.columnName).data = noDuplicatedKey2;
+            table.AddRow(row);
+            //Test
+            row = table.CreateRowDefinition();
+            row.GetCell(pkColumn1.columnName).data = noDuplicatedKey1;
+            row.GetCell(pkColumn2.columnName).data = noDuplicatedKey2;
+            Assert.IsFalse(table.primaryKey.Evaluate(row));
+        }
+
+        [TestMethod]
+        public void XtremeCase_TwoColumnAsPK_NewRowWithNoPKDuplicated_ButIndividuallyPKColumnsValuesAreDuplicated_EvalReturnTrue()
+        {
+            //Construct phase
+            Table table = CreatePrimaryKeysTable();
+            Column pkColumn1 = table.GetColumn(testTableColumns[0]);
+            Column pkColumn2 = table.GetColumn(testTableColumns[1]);
+            table.primaryKey.AddKey(pkColumn1);
+            table.primaryKey.AddKey(pkColumn2);
+            string noDuplicatedKey1 = ShittyUtilities.GenerateADoenstExistNewValue(pkColumn1);
+            string noDuplicatedKey2 = ShittyUtilities.GenerateADoenstExistNewValue(pkColumn2);
+            Row row = table.CreateRowDefinition();
+            row.GetCell(pkColumn1.columnName).data = noDuplicatedKey1;
+            row.GetCell(pkColumn2.columnName).data = noDuplicatedKey2;
+            table.AddRow(row);
+            string noDuplicatedKey3 = ShittyUtilities.GenerateADoenstExistNewValue(pkColumn1);
+            string noDuplicatedKey4 = ShittyUtilities.GenerateADoenstExistNewValue(pkColumn2);
+            row = table.CreateRowDefinition();
+            row.GetCell(pkColumn1.columnName).data = noDuplicatedKey3;
+            row.GetCell(pkColumn2.columnName).data = noDuplicatedKey4;
+            table.AddRow(row);
+            //Test
+            row = table.CreateRowDefinition();
+            row.GetCell(pkColumn1.columnName).data = noDuplicatedKey1;
+            row.GetCell(pkColumn2.columnName).data = noDuplicatedKey4;
+            Assert.IsTrue(table.primaryKey.Evaluate(row));
+        }
+
         private static Table CreatePrimaryKeysTable()
         {
             Table table = new Table(tableName);
